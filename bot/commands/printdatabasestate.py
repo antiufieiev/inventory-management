202862e7fd9b0e09c5ca893f297e3d166f9b5167
shmukli <wellplayedbb@gmail.com>
2023-05-06 15:29:57 +0200

    [IMB-16] Changed report format

--- bot/commands/printdatabasestate.py
+++ bot/commands/printdatabasestate.py
@@ -1,6 +1,6 @@
 from bot.commands.basecommand import *
 from bot.commands.default_fallback import *
-from bot.database.model import Batches, database_proxy
+from bot.database.model import Batches, Packaging, database_proxy
 from bot.feature.permissionchecker import checkUserAccess
 from bot.localization.localization import *
 
@@ -18,21 +18,30 @@ class DatabaseStateCommand(BaseConversation):
             packed = ''
             unpacked = ''
             with database_proxy.connection_context():
-                for batch in Batches.select():
-                    line = localization_map[Keys.PRINT_DATABASE_STATE_LINE].format(
-                        batch.cheese.name,
-                        batch.batch_number,
-                        str(batch.count),
-                        batch.comment
-                    )
+                for batch in Batches.select(Batches):
                     if batch.packaging is not None:
+                        line = localization_map[Keys.PRINT_DATABASE_STATE_LINE_PACKED].format(
+                            batch.cheese.name,
+                            batch.packaging.packaging,
+                            str(batch.count),
+                            batch.comment
+
+                        )
                         packed += line
                     if batch.packaging is None:
-                        unpacked += line
+                        line_unpacked = localization_map[Keys.PRINT_DATABASE_STATE_LINE].format(
+                            batch.cheese.name,
+                            batch.batch_number,
+                            str(batch.count),
+                            batch.comment
+                        )
+                        unpacked += line_unpacked
+
             await context.bot.send_message(
                 chat_id=update.effective_chat.id,
                 text=localization_map[Keys.PRINT_DATABASE_STATE].format(packed, unpacked)
             )
+
         else:
             await update.effective_message.reply_text(
                 text=localization_map[Keys.ACCESS_DENIED],
